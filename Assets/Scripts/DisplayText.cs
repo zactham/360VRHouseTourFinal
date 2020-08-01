efd9574476821d8079fa19c1a784471af572aa0b
zactham <24833352+zactham@users.noreply.github.com>
2020-08-01 00:18:21 -0400

    working

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour
+{
+    //Takes in a game object and toggles
+    public void displayText(GameObject TextMesh)
+    {
+        TextMesh.SetActive(!TextMesh.activeSelf);
+    }
+
+    public void hideText(GameObject TextMesh)
+    {
+        TextMesh.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo : MonoBehaviour
+{
+    public GameObject maincam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 50f;
+        //transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        //transform.rotation = Quaternion.RotateTowards(maincam.transform.rotation, Quaternion.Euler(0, 180, 0), speed * Time.deltaTime);
+        //this.transform.Rotate(0, maincam.transform.position.y, 0, Space.Self);
+    }
+}
